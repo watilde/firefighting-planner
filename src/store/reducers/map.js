88ba663c9644b939f3b7b650801fdabdec2496f2
Daijiro Wachi <daijiro.wachi@gmail.com>
2019-10-25 16:13:36 +0900

    add icon

--- src/store/reducers/map.js
+++ src/store/reducers/map.js
@@ -3,6 +3,13 @@ export const mapState = {
   cordinates: false,
   viewControls: false,
   threeD: false,
+  BMNG: true,
+  BMNGLandsat: false,
+  BingAerial: false,
+  BingAerialWithLabels: true,
+  BingRoads: false,
+  OpenStreetMapImage: false,
+  Atmosphere: true,
   layer: { label: "Select leyer", tag: "_" }
 };
 
@@ -36,6 +43,48 @@ export default (state = mapState, action) => {
       return {
         ...state,
         viewControls: !state.viewControls
+      };
+    }
+    case "@map/toggleBMNG": {
+      return {
+        ...state,
+        BMNG: !state.BMNG
+      };
+    }
+    case "@map/toggleBMNGLandsat": {
+      return {
+        ...state,
+        BMNGLandsat: !state.BMNGLandsat
+      };
+    }
+    case "@map/toggleBingAerial": {
+      return {
+        ...state,
+        BingAerial: !state.BingAerial
+      };
+    }
+    case "@map/toggleBingAerialWithLabels": {
+      return {
+        ...state,
+        BingAerialWithLabels: !state.BingAerialWithLabels
+      };
+    }
+    case "@map/toggleBingRoads": {
+      return {
+        ...state,
+        BingRoads: !state.BingRoads
+      };
+    }
+    case "@map/toggleOpenStreetMapImage": {
+      return {
+        ...state,
+        OpenStreetMapImage: !state.OpenStreetMapImage
+      };
+    }
+    case "@map/toggleAtmosphere": {
+      return {
+        ...state,
+        Atmosphere: !state.Atmosphere
       };
     }
     default: {
