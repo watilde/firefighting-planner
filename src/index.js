00a0d907ee6e83a2a2ffc7ac6d1e0a3bf3d4fd35
Daijiro Wachi <daijiro.wachi@gmail.com>
2019-10-26 14:04:29 +0900

    change color

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/core/styles";
 import "./index.css";
+import theme from "./theme";
 import App from "./App";
 import { StoreProvider } from "./store/configureStore";
 import * as serviceWorker from "./serviceWorker";
@@ -9,7 +11,9 @@ import * as serviceWorker from "./serviceWorker";
 ReactDOM.render(
   <StoreProvider>
     <CssBaseline />
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StoreProvider>,
   document.getElementById("root")
 );
---
+++ src/theme.js
@@ -0,0 +1,18 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const orange = "rgba(248, 102, 36, 1)";
+const pink = "rgba(234, 53, 70, 1)";
+const yellow = "rgba(249, 200, 14, 1)";
+const blue = "rgba(67, 188, 205, 1)";
+const purple = "rgba(102, 46, 155, 1)";
+
+export default createMuiTheme({
+  palette: {
+    primary: {
+      main: "#ef6c00"
+    },
+    secondary: {
+      main: "#0d47a1"
+    }
+  }
+});
