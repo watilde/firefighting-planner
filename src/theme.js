62ca28986823034403346cec47c5a283b4bd953a
Daijiro Wachi <daijiro.wachi@gmail.com>
2019-10-26 14:07:47 +0900

    remove unused variables

--- src/theme.js
+++ src/theme.js
@@ -1,11 +1,5 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 
-const orange = "rgba(248, 102, 36, 1)";
-const pink = "rgba(234, 53, 70, 1)";
-const yellow = "rgba(249, 200, 14, 1)";
-const blue = "rgba(67, 188, 205, 1)";
-const purple = "rgba(102, 46, 155, 1)";
-
 export default createMuiTheme({
   palette: {
     primary: {
