2bdd71a9d5a3f6b92e9349b0bc7f1f26027ae567
Daijiro Wachi <daijiro.wachi@gmail.com>
2019-12-03 02:09:54 +0900

    fix: use normal ga tag

--- src/index.js
+++ src/index.js
@@ -8,10 +8,6 @@ import theme from "./theme";
 import App from "./App";
 import { StoreProvider } from "./store/configureStore";
 import * as serviceWorker from "./serviceWorker";
-import ReactGA from "react-ga";
-
-ReactGA.initialize("UA-35821550-9");
-ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <StoreProvider>
